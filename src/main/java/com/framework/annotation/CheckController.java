59255075125627985c23e942cbcf1ee925803aa4
Volatiana Estelle <estelleherimanana4@gmail.com>
2026-06-21 16:48:28 +0300

    modif

--- src/main/java/com/framework/annotation/CheckController.java
+++ src/main/java/com/framework/annotation/CheckController.java
@@ -1,5 +1,6 @@
 package com.framework.annotation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.framework.util.Utilitaire;
@@ -13,5 +14,20 @@ public class CheckController {
                 System.out.println(c.getName());
             }
         }
+    }
+
+    public List<Class<?>> getListController(String pack) throws Exception {
+        List<Class<?>> controllers = new ArrayList<>();
+        List<Class<?>> classes = Utilitaire.getClasses(pack);
+        for (Class<?> c : classes) {
+            if (c.isAnnotationPresent(Controller.class)) {
+                controllers.add(c);
+            }
+        }
+        return controllers;
+    }
+
+    public List<Class<?>> getListController() throws Exception {
+        return getListController("controller");
     }
 } 
--- src/main/java/com/framework/servlet/FrontControllerServlet.java
+++ src/main/java/com/framework/servlet/FrontControllerServlet.java
@@ -15,42 +15,85 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 
 public class FrontControllerServlet extends HttpServlet {
-    List<String> listController = new ArrayList<>();
+    private List<String> listController = new ArrayList<>();
+
+    @Override
+    public void init() throws ServletException {
+
+        try {
+
+            System.out.println("=== FRAMEWORK INIT ===");
+            System.out.println("############################");
+            System.out.println("VERSION FRAMEWORK 999999");
+            System.out.println("############################");
+
+            ServletContext context = getServletContext();
+
+            String pack = context.getInitParameter("controller");
+
+            System.out.println("Package = " + pack);
+
+            List<Class<?>> classes = Utilitaire.getClasses(pack);
+
+            System.out.println("Nombre classes = "+ classes.size());
+
+            for (Class<?> c : classes) {
+
+                System.out.println("Classe trouvée = "+ c.getName());
+
+                System.out.println("Controller ? "+ c.isAnnotationPresent(com.framework.annotation.Controller.class));
+
+                if (c.isAnnotationPresent(com.framework.annotation.Controller.class)) {
+                    listController.add(c.getName());
+                }
+            }
+
+            for (Class<?> c : classes) {
+                if (c.isAnnotationPresent(com.framework.annotation.Controller.class)) {
+                    listController.add(c.getName());
+                }
+            }
+
+            System.out.println("Controllers trouvés : " + listController.size());
+
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        captureUrl(request, response);
+        processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        captureUrl(request, response);
+        processRequest(request, response);
     }
 
-    private void captureUrl(HttpServletRequest request, HttpServletResponse response)
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter out = response.getWriter();
 
+        out.println("wertyuiop");
         String url = request.getRequestURI();
 
         out.println(url);
+
+        for (String string : listController) {
+            out.println(string);
+        }
     }
 
-    @Override
-    public void init() throws ServletException {
-        try {
-            ServletContext context = getServletContext();
-            String pack = context.getInitParameter("controller");
-            
-            System.out.println("===============");
-            System.out.println("package: " + pack);
+    public List<String> getListController() {
+        return listController;
+    }
 
-            CheckController.getControllers(pack);
-        } catch (Exception e) {
-            throw new ServletException("Error initializing FrontControllerServlet", e);
-        }
+    public void setListController(List<String> listController) {
+        this.listController = listController;
     }
 
 }
